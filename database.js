0be58a21a60b1bcfb7e317dd69bc2c7743dd7c75
LuigiAnaclet <luigi.anaclet@supinfo.com>
2023-12-31 02:23:51 +0100

    database

--- database.js
+++ database.js
@@ -1,11 +1,9 @@
 const { Pool } = require('pg');
-
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'procuratio_db',
-    password: 'Stanley97231!',
-    port: 5432
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
-
 module.exports = pool;
